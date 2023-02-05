ad45cbd13c1f902b917c1affe84071884f19156d
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-05 17:38:26 +0530

    array declaration, initialization and access array elements

--- src/Arrays/ArrayDeclaration.java
+++ src/Arrays/ArrayDeclaration.java
@@ -1,5 +1,19 @@
 package Arrays;
 
 public class ArrayDeclaration {
+public static void main(String args[]){
+//    method 01
+    int ages[] = {20,30,40,70,80};
+    System.out.println(ages[0]);
 
+//    method 02
+    String names[]=new String[6];
+    names[0] = "Rashmiya";
+    names[1] = "Risni";
+    names[2] = "Fareena";
+    names[3] = "Rafeek";
+    names[4] = "Rafa";
+    names[5] = "Raizha";
+    System.out.println(names[3]);
+}
 }
