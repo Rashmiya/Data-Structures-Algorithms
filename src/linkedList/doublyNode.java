2bde5d4532b1d1acf8b76ec1d37e01712260dc98
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-05 22:27:44 +0530

    introduced doubly linked list

---
+++ src/linkedList/doublyNode.java
@@ -0,0 +1,14 @@
+package linkedList;
+
+public class doublyNode {
+    int val;
+    doublyNode next;
+    doublyNode previous;
+
+    // create the constructor
+    public doublyNode(int val,doublyNode next, doublyNode previous){
+        this.val = val;
+        this.next = next;
+        this.previous = previous;
+    }
+}
